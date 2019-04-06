853d9d9225699fe832a62d4c6b0585a1c037edb7
megis7 <megisoglou56@gmail.com>
2019-04-06 11:52:36 +0300

    Added tests for instruction E decoding - description in readme.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # risky
+
+A 32-bit risc-v processor implementation in [clash](https://clash-lang.org/).
+
+## Features
+
+Encoding/Decoding of instructions of the RV32I specification.
+
+## How to install
+
+Clone the project and then execute:
+```
+stack build
+```
+
+This will install the clash environment and all the required haskell packages.
+
+## How to run
+
+### REPL
+```
+stack exec -- clashi src/System.hs -isrc -odir obj -hidir obj
+```
+
+### Generate verilog
+```
+stack exec -- clash src/System.hs -isrc --verilog -odir obj -hidir obj
+```
+
+### Test Suite
+```
+stack exec -- runhaskell test/Spec.hs
+```
--- test/Spec.hs
+++ test/Spec.hs
@@ -23,34 +23,33 @@ import Data.Bool
 
 import Clash.Prelude
 
-registers = Registers { general = repeat (-1) :: Vec 32 XTYPE, pc = 0 :: XTYPE}
+registers = Registers { general = 1 :> 0 :> 10 :> repeat (-1) :: Vec 32 XTYPE, pc = 8 :: XTYPE}
 
-topEntity = execute decE
-    where
-        x = encodeInstruction $ Itype ADD (Register 1) (Register 2) 10
-        decD = decodeInstruction x
-        decE = decodeInstructionE registers decD
-
-encodeInstructionResult = [0b00000000000111000111000100110111 :: BitVector 32] :> Nil   -- ! TODO: Fix this so that all instruction read result from here
-
-instrArray =  (Utype LUI (Register 2) 0b000111000111,                         0b00000000000111000111000100110111 :: BitVector 32) :> 
-              (Utype AUIPC (Register 31) 0b000111000111,                      0b00000000000111000111111110010111) :>
-              (Jtype JAL (Register 15) 0b11110000111100001111,                0b11100001111111100001011111101111) :>
-              (Itype JALR (Register 1) (Register 2) 26,                       0b00000001101000001000000101100111) :>
-              (Branch BEQ (Register 10) (Register 20) 0b101010101010,         0b11010101010001010000101001100011) :>
-              (Branch BGEU (Register 10) (Register 20) 0b101010101010,        0b11010101010001010111101001100011) :>
-              (Itype SLT (Register 5) (Register 15) 0b111100001111,           0b11110000111100101010011110010011) :> 
-              (Itype SL (Register 5) (Register 15) 0b000000010001,            0b00000001000100101001011110010011) :>
-              (Itype SRL (Register 5) (Register 15) 0b000000010001,           0b00000001000100101101011110010011) :>
-              (Itype SRA (Register 5) (Register 15) 0b010000010001,           0b01000001000100101101011110010011) :>
-              (Rtype XOR (Register 5) (Register 1) (Register 15) 0,           0b00000000000100101100011110110011) :>
-              (Rtype SUB (Register 5) (Register 1) (Register 15) 0b0100000,   0b01000000000100101000011110110011) :>
-              (Rtype SRA (Register 5) (Register 1) (Register 15) 0b0100000,   0b01000000000100101101011110110011) :>
-              (Rtype SRL (Register 5) (Register 1) (Register 15) 0,           0b00000000000100101101011110110011) :>
+instrArray =  (0b00000000000111000111000100110111,  Utype LUI (Register 2) 0b000111000111,                       UtypeE LUI 1863680 8 (Register 2))          :> 
+              (0b00000000000111000111111110010111,  Utype AUIPC (Register 31) 0b000111000111,                    UtypeE AUIPC 1863680 8 (Register 31))       :>
+              (0b11100001111111100001011111101111,  Jtype JAL (Register 15) 0b11110000111100001111,              JumpE JAL (-123362) 8 (Register 15))        :>
+              (0b00000001101000001000000101100111,  Itype JALR (Register 1) (Register 2) 26,                     JumpE JALR 26 8 (Register 2))               :>
+              (0b11010101010001010000101001100011,  Branch BEQ (Register 10) (Register 20) 0b101010101010,       BranchE BEQ (-1) (-1) 8 (-2732))            :>
+              (0b11010101010001010111101001100011,  Branch BGEU (Register 10) (Register 20) 0b101010101010,      BranchE BGEU (-1) (-1) 8 (-2732))           :>
+              (0b11110000111100101010011110010011,  Itype SLT (Register 5) (Register 15) 0b111100001111,         ArithmeticE SLT (-1) (-241) (Register 15))  :> 
+              (0b00000001000100101001011110010011,  Itype SL (Register 5) (Register 15) 0b000000010001,          ArithmeticE SL (-1) 17 (Register 15))       :>
+              (0b00000001000100101101011110010011,  Itype SRL (Register 5) (Register 15) 0b000000010001,         ArithmeticE SRL (-1) 17 (Register 15))      :>
+              (0b01000001000100101101011110010011,  Itype SRA (Register 5) (Register 15) 0b010000010001,         ArithmeticE SRA (-1) 1041 (Register 15))    :>
+              (0b00000000000100101100011110110011,  Rtype XOR (Register 5) (Register 1) (Register 15) 0,         ArithmeticE XOR (-1) 0 (Register 15))       :>
+              (0b01000000000100101000011110110011,  Rtype SUB (Register 5) (Register 1) (Register 15) 0b0100000, ArithmeticE SUB (-1) 0 (Register 15))       :>
+              (0b01000000000100101101011110110011,  Rtype SRA (Register 5) (Register 1) (Register 15) 0b0100000, ArithmeticE SRA (-1) 0 (Register 15))       :>
+              (0b00000000000100101101011110110011,  Rtype SRL (Register 5) (Register 1) (Register 15) 0,         ArithmeticE SRL (-1) 0 (Register 15))       :>
               Nil
 
-checkEncode i = encodeInstruction (fst (instrArray !! i)) `shouldBe` (snd (instrArray !! i))
-checkDecodeD i = decodeInstruction (snd (instrArray !! i)) `shouldBe` (fst (instrArray !! i))
+sfst (x,_,_) = x
+ssnd (_,x,_) = x
+strd (_,_,x) = x
+
+decE = decodeInstructionE registers
+
+checkEncode i = encodeInstruction (ssnd el) `shouldBe` (sfst el)    where el = instrArray !! i
+checkDecodeD i = decodeInstruction (sfst el) `shouldBe` (ssnd el)   where el = instrArray !! i
+checkDecodeE i = decE (ssnd el) `shouldBe` (strd el)                where el = instrArray !! i  
 
 main :: IO ()
 main = hspec $ do
@@ -87,5 +86,17 @@ main = hspec $ do
     it "decodes SRL"    $ do   checkDecodeD 13
 
   describe "Core.Decode.decodeE" $ do
-    it "needs testing" $ do
-      pending
+    it "decodes LUI"    $ do   checkDecodeE 0
+    it "decodes AUIPC"  $ do   checkDecodeE 1
+    it "decodes JAL"    $ do   checkDecodeE 2
+    it "decodes JALR"   $ do   checkDecodeE 3
+    it "decodes BEQ"    $ do   checkDecodeE 4
+    it "decodes BGEU"   $ do   checkDecodeE 5
+    it "decodes SLTI"   $ do   checkDecodeE 6
+    it "decodes SLLI"   $ do   checkDecodeE 7
+    it "decodes SRLI"   $ do   checkDecodeE 8
+    it "decodes SRAI"   $ do   checkDecodeE 9
+    it "decodes XOR"    $ do   checkDecodeE 10
+    it "decodes SUB"    $ do   checkDecodeE 11
+    it "decodes SRA"    $ do   checkDecodeE 12
+    it "decodes SRL"    $ do   checkDecodeE 13
