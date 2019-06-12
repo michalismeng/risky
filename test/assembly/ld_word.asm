e8c4f337bf002a9c5fc5d53631d8ff079138e41e
megis7 <megisoglou56@gmail.com>
2019-06-12 21:43:05 +0300

    Fixed minor stuff - added base addresses for text/data segments.

--- test/assembly/ld_word.asm
+++ test/assembly/ld_word.asm
@@ -6,12 +6,12 @@
 	
 .text
 main:
-	addi x1, x0, 0
+	la x1, val
 	lw x2, 0(x1)
-	lw x3, 4(x1)
+	# lw x3, 4(x1)
 	
 	addi x1, x1, 4
 	lw x4, -4(x1)
-	lw x5, 0(x1)
+	#lw x5, 0(x1)
 	
 halt:	beq x0, x0, halt
